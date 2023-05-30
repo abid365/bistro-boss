285ee1545598596f6d570ddec175480500fa5d1d
jawad Bin Azam <abidibnazam@gmail.com>
2023-05-30 21:41:37 +0600

    added pagination but incomplete

--- src/Pages/Order/OrderTab/OrderTab.jsx
+++ src/Pages/Order/OrderTab/OrderTab.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import FoodCard from "../../../Components/foodCard/FoodCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 const OrderTab = ({ items }) => {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  };
   return (
-    <div className="grid md:grid-cols-3 gap-10">
-      {items.map((item) => (
-        <FoodCard item={item} key={item._id}></FoodCard>
-      ))}
+    <div>
+      <Swiper
+        pagination={pagination}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="grid md:grid-cols-3 gap-10">
+            {items.map((item) => (
+              <FoodCard item={item} key={item._id}></FoodCard>
+            ))}
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
